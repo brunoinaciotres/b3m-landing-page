9c085ff9a68cf8cf1b461cde12e2dd488c94b8b1
Bruno Tres <btres76@gmail.com>
2025-01-17 13:41:54 -0300

    changes smtp provider

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -4,18 +4,18 @@ import NM from 'nodemailer'
 export async function POST(req) {
 
     const { name, email, msg } = await req.json()
-
-    const user = "b3massessoria@gmail.com"
+    
+    const user = "MS_oLV16r@trial-jpzkmgqwn32g059v.mlsender.net"
     const pass = process.env.EMAIL_PASS
 
     const transporter = NM.createTransport({
-        host: "smtp.gmail.com",
-        port: "465",
+        host: "smtp.mailersend.net",
+        port: "587",
         auth: { user, pass }
     })
 
     return transporter.sendMail({
-        from: "b3massessoria@gmail.com",
+        from: "MS_oLV16r@trial-jpzkmgqwn32g059v.mlsender.net",
         to: "b3massessoria@gmail.com",
         subject: "Contato do Site",
         text: `Nome: ${name}. Email: ${email}, Mensagem: ${msg}`,

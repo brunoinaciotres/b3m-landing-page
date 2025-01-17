2c4287fc691eb19c6ca27985c4e81758aaff996d
Bruno Tres <btres76@gmail.com>
2025-01-16 21:08:21 -0300

    create link for messaging on whatsapp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -81,7 +81,7 @@ export default function Home() {
             <img src="/images/whatsapp.png" alt="ícone do Whatsapp" />
             <span>Whatsapp</span>
             <span className="wpp-number fw-bold">(32) 99160-7447</span>
-            <a href="#">Clique para enviar uma mensagem</a>
+            <a href="https://wa.me/5532991463499/?text=Olá, gostaria de mais informações">Clique para enviar uma mensagem</a>
           </div>
           <span className="fs-5 text-contact">Ou nos envie um email</span>
           <form action="#" className="send-email">

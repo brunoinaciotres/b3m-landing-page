1537f288ba1a73bd4ce9195422380ef4afdda005
Bruno Tres <btres76@gmail.com>
2025-01-20 15:43:43 -0300

    unable some buttons

--- src/components/Text1/Text1.jsx
+++ src/components/Text1/Text1.jsx
@@ -28,7 +28,7 @@ export default function Text1() {
             <div className="d-flex text-1-container ">
                 <div className="d-flex flex-column text-1-paragraph-container">
                     <p>Transformamos sua <span className="fw-bold">presença digital</span> em um <span className="fw-bold">diferencial competitivo</span>: do gerenciamento estratégico do seu Instagram à criação de sites e designs personalizados, trazemos soluções completas para destacar sua empresa na sua cidade e além!</p>
-                    <button className="outlined-btn ">Entenda como</button>
+                    {/* <button className="outlined-btn ">Entenda como</button> */}
                 </div>
                 <div className="phone-image-container d-flex align-center justify-content-center">
                     <img src="/images/phone.png" className="w-100" alt="Ilustração de um celular com icones 3D flutuando em sua frente fora da tela" />
